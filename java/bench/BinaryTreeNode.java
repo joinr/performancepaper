8bfae495e7e489f266fc299e211193e9d278961d
tom <tom@that-place>
2020-09-07 11:56:15 -0500

    Added aux java files

---
+++ java/bench/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+package bench;
+
+//BinaryTreeNode not provided by the author....
+//joinr's interpretation of BinaryTreeNode
+public class BinaryTreeNode{
+    public int value;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+    public BinaryTreeNode(int v){
+        value = v;
+    }
+}
---
+++ java/bench/LLNode.java
@@ -0,0 +1,7 @@
+package bench;
+
+public class  LLNode{
+    public  LLNode  next ;
+    public  LLNode (LLNode next){
+        this.next  =  next ;}
+}
